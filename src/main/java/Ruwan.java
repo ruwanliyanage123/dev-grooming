d385ea534d1e0f389dfd8539148f1b3fc6fbe233
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-08-03 20:22:59 +0530

    Revert "tesst"
    This reverts commit da3e1f4cc3f56dd99e2b1e0a08cde90c9e8ddba9.

--- src/main/java/Ruwan.java
+++ src/main/java/Ruwan.java
@@ -1,7 +1,5 @@
 public class Ruwan {
     public static void main(String[] args) {
         System.out.println("made by ruwan");
-        System.out.println("made by ruwan");
-        System.out.println("made by ruwan");
     }
 }
