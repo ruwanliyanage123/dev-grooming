494a880dff00da6939e41af384a8565654291faa
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-08-03 19:55:34 +0530

    commit in prod

---
+++ src/main/java/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello, World by ruwan 1");
+        System.out.println("Hello, World by ruwan 2");
+        System.out.println("Hello, World by ruwan 3");
+        System.out.println("Hello, World by ruwan 4");
+    }
+}
