7e5e8e0e6a65fa439f6e958c69970e456c52fa5c
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-08-03 19:25:26 +0530

    commit by ruwan 3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World by ruwan");
+        System.out.println("Hello, World by ruwan 1");
+        System.out.println("Hello, World by ruwan 2");
     }
 }
